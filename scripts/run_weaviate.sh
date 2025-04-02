645a193a9ddbb053f8c4b60488166eed324aec62
Jakub Kucera <jakub.kucerja@gmail.com>
2025-04-02 17:33:34 +0200

    Fix run Weaviate instance on Slurm nodes

--- scripts/run_weaviate.sh
+++ scripts/run_weaviate.sh
@@ -7,5 +7,7 @@
 #unset CLUSTER_HOSTNAME
 
 cd ~/weaviate
+# remove raft dir - needed so that this can be run from any Cluster FIT directories
+rm -rf ~/weaviate/data/raft
 #~/weaviate/weaviate-server --host 0.0.0.0 --port 8080 --scheme HTTP
 env -i ~/weaviate/weaviate-server --host 0.0.0.0 --port 8080 --scheme http
