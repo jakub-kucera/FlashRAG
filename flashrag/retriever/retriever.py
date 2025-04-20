6a437e87e772ecd5b613730e08459994d0d23128
Jakub Kucera <jakub.kucerja@gmail.com>
2025-04-20 14:03:12 +0200

    add id to docs retrieved from Weaviate

--- flashrag/retriever/retriever.py
+++ flashrag/retriever/retriever.py
@@ -548,6 +548,7 @@ class WeaviateRetriever(BaseTextRetriever):
 
         for o in response.objects:
             doc = {
+                'id': o.properties['orig_id'],
                 'title': o.properties['name'],
                 'text': o.properties['contents'],
                 **o.properties
